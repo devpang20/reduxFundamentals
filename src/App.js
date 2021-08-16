2ab3ab581c92575744be79c65bb6d391af197b08
sik2 <devpang20@gmail.com>
2021-08-16 13:02:03 +0900

    feat:add point up redux excercise

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Provider } from "react-redux";
+import Point from "./Point";
+import store from "./redux/store";
 
 const App = () => {
-  return <div> </div>;
+  return (
+    <Provider store={store}>
+      <div>
+        <Point />
+      </div>
+    </Provider>
+  );
 };
 
 export default App;
