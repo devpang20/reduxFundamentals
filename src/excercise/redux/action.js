b7c5ce26a3958056b297e12d6a93f4456e04264e
sik2 <devpang20@gmail.com>
2021-08-22 02:52:47 +0900

    refactor: delete legacy code

--- src/excercise/redux/action.js
+++
@@ -1,7 +0,0 @@
-const ADD_POINT = "ADD_POINT";
-
-export const addPoint = () => {
-  return {
-    type: ADD_POINT,
-  };
-};
--- src/excercise/redux/store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import pointReducer from "./reducer";
-
-const store = createStore(pointReducer);
-
-export default store;
